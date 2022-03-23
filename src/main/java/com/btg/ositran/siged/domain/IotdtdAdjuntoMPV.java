950cbee22288d204767449cf3028fc3075bd706e
Admin <Admin>
2022-03-23 15:48:38 -0500

    Se corrigio duplicidad archivo principal

--- src/main/java/com/btg/ositran/siged/domain/IotdtdAdjuntoMPV.java
+++ src/main/java/com/btg/ositran/siged/domain/IotdtdAdjuntoMPV.java
@@ -2,92 +2,103 @@ package com.btg.ositran.siged.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name = "IOTDTD_ADJUNTO_MPV")
 @XmlRootElement
+@NamedQueries({
+	@NamedQuery(name = "IotdtdAdjuntoMPV.buscarTodos", query = "SELECT i FROM IotdtdAdjuntoMPV i"),
+	@NamedQuery(name = "IotdtdAdjuntoMPV.buscarPorIdAdjunto", query = "SELECT i FROM IotdtdAdjuntoMPV i WHERE i.idAdjunto = :idAdjunto"),
+	@NamedQuery(name = "IotdtdAdjuntoMPV.buscarNumFoliosPorIdRecepcion", query = "SELECT i.numeroFolios FROM IotdtdAdjuntoMPV i WHERE i.recepcion.sidrecext = :idRecepcion and i.tipoArchivo = :tipoArchivo"),
+	@NamedQuery(name = "IotdtdAdjuntoMPV.buscarNumFoliosTotalesPorIdRecepcion", query = "SELECT sum(i.numeroFolios) FROM IotdtdAdjuntoMPV i WHERE i.recepcion.sidrecext = :idRecepcion group by i.recepcion.sidrecext")
+})
 public class IotdtdAdjuntoMPV implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@Basic(optional = false)
 	@Column(name = "IDADJUNTOMPV")
-	private Integer idadjuntompv;
+	private Integer idAdjunto;
 
 	@Basic(optional = false)
 	@Column(name = "TIPOARCHIVO")
-	private String tipoarchivo;
+	private Integer tipoArchivo;
 
 	@Basic(optional = false)
 	@Column(name = "NOMBREARCHIVO")
-	private String nombrearchivo;
+	private String nombreArchivo;
 
 	@Basic(optional = false)
 	@Column(name = "RUTAARCHIVO")
-	private String rutaarchivo;
+	private String rutaArchivo;
 
 	@Basic(optional = false)
 	@Column(name = "FECHAREGISTRO")
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date fecharegistro;
+	private Date fechaRegistro;
 
 	@JoinColumn(name = "IDRECEPCION", referencedColumnName = "SIDRECEXT")
 	@ManyToOne(optional = false)
 	private IotdtcRecepcionMPV recepcion;
 	
+	@Column(name = "NUMEROFOLIOS")
+	private Integer numeroFolios;
 
 	public IotdtdAdjuntoMPV() {
 	}
 
-	public Integer getIdadjuntompv() {
-		return idadjuntompv;
+	public Integer getIdAdjunto() {
+		return idAdjunto;
 	}
 
-	public void setIdadjuntompv(Integer idadjuntompv) {
-		this.idadjuntompv = idadjuntompv;
+	public void setIdAdjunto(Integer idAdjunto) {
+		this.idAdjunto = idAdjunto;
 	}
 
-	public String getTipoarchivo() {
-		return tipoarchivo;
+	public Integer getTipoArchivo() {
+		return tipoArchivo;
 	}
 
-	public void setTipoarchivo(String tipoarchivo) {
-		this.tipoarchivo = tipoarchivo;
+	public void setTipoArchivo(Integer tipoArchivo) {
+		this.tipoArchivo = tipoArchivo;
 	}
 
-	public String getNombrearchivo() {
-		return nombrearchivo;
+	public String getNombreArchivo() {
+		return nombreArchivo;
 	}
 
-	public void setNombrearchivo(String nombrearchivo) {
-		this.nombrearchivo = nombrearchivo;
+	public void setNombreArchivo(String nombreArchivo) {
+		this.nombreArchivo = nombreArchivo;
 	}
 
-	public String getRutaarchivo() {
-		return rutaarchivo;
+	public String getRutaArchivo() {
+		return rutaArchivo;
 	}
 
-	public void setRutaarchivo(String rutaarchivo) {
-		this.rutaarchivo = rutaarchivo;
+	public void setRutaArchivo(String rutaArchivo) {
+		this.rutaArchivo = rutaArchivo;
 	}
 
-	public Date getFecharegistro() {
-		return fecharegistro;
+	public Date getFechaRegistro() {
+		return fechaRegistro;
 	}
 
-	public void setFecharegistro(Date fecharegistro) {
-		this.fecharegistro = fecharegistro;
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
 	}
 
 	public IotdtcRecepcionMPV getRecepcion() {
@@ -98,4 +109,18 @@ public class IotdtdAdjuntoMPV implements Serializable {
 		this.recepcion = recepcion;
 	}
 
+	public Integer getNumeroFolios() {
+		return numeroFolios;
+	}
+
+	public void setNumeroFolios(Integer numeroFolios) {
+		this.numeroFolios = numeroFolios;
+	}
+
+	@Override
+	public String toString() {
+		return "IotdtdAdjuntoMPV [idAdjunto=" + idAdjunto + ", tipoArchivo=" + tipoArchivo + ", nombreArchivo="
+			+ nombreArchivo + ", rutaArchivo=" + rutaArchivo + ", fechaRegistro=" + fechaRegistro + ", recepcion="
+			+ recepcion + ", numeroFolios=" + numeroFolios + "]";
+	}
 }
